54cd828d1c1f6a7b1e352865b40a5296c1fb136a
Akshat Vyas <akshatvyas110@gmail.com>
2024-07-07 12:27:59 +0530

    Chapter 2.1

---
+++ src/main/java/com/week2lectures/introductionToMVCarchitecture/IntroductionToMVCArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.week2lectures.introductionToMVCarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToMVCArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToMVCArchitectureApplication.class, args);
+	}
+
+}
